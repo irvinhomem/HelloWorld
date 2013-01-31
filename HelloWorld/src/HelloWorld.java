cca237d8c046250599c5cce82bbe62ab7beffa19
irvin <ihomem@gmail.com>
2013-01-31 22:43:44 +0100

    Adding files and removing files from "index"

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author irvin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Testing git!");
+	}
+
+}
