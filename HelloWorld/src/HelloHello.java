64c08e43beac43dea564db8e0bb55c86a63a92b0
irvin <ihomem@gmail.com>
2013-01-31 22:48:16 +0100

    Added HelloHello class

---
+++ HelloWorld/src/HelloHello.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author irvin
+ *
+ */
+public class HelloHello {
+
+	/**
+	 * 
+	 */
+	public HelloHello() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Hello Hello");
+	}
+
+}
